dc698d39473543c8dcd7da9fed63d1e4ee64db5c
JunHahn <21152231+TangoEnSkai@users.noreply.github.com>
2019-08-15 02:34:44 +0900

    Add code for the client

---
+++ 02-online-courses/grpc-modern-api-microservices/04-grpc-project-overview-setup/codes/greet/greet_client/client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"../greetpb"
+	"google.golang.org/grpc"
+)
+
+
+func main() {
+	fmt.Println("Hello, I am a client.")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())	// WithInsecure() for just now testing
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+}
