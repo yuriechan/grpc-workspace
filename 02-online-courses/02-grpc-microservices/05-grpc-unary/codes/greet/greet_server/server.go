42826ce9876c81a331fbb7004a5bf15954825f51
JunHahn <21152231+TangoEnSkai@users.noreply.github.com>
2019-08-18 21:39:50 +0900

    Update notes and codes for lecture 20, unary api server implementation

--- 02-online-courses/02-grpc-microservices/05-grpc-unary/codes/greet/greet_server/server.go
+++ 02-online-courses/02-grpc-microservices/05-grpc-unary/codes/greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -12,6 +13,15 @@ import (
 
 type server struct{}
 
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
 func main() {
 	fmt.Println("Hello world!")
 
@@ -21,7 +31,7 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	greetpb.RegisterGreetServiceServer(s, server{})
+	greetpb.RegisterGreetServiceServer(s, &server{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
