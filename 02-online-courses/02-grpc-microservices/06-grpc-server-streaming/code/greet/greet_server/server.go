2fca69483851c29c8a83b04b89e51cddd4836329
TangoEnSkai <21152231+TangoEnSkai@users.noreply.github.com>
2019-10-09 02:28:21 +0900

    update import

--- 02-online-courses/02-grpc-microservices/06-grpc-server-streaming/code/greet/greet_server/server.go
+++ 02-online-courses/02-grpc-microservices/06-grpc-server-streaming/code/greet/greet_server/server.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 	"log"
 	"net"
+	"strconv"
 
 	"../greetpb"
 
@@ -21,6 +23,20 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 		Result: result,
 	}
 	return res, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("GreetManyTimes function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstName + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
 }
 
 func main() {
