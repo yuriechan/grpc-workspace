d8b012086fa6845c0e97e63d27dae86a4fa5b1c3
TangoEnSkai <21152231+TangoEnSkai@users.noreply.github.com>
2019-11-24 22:11:44 +0900

    add all codes for session 7 client streaming with calculator example

--- 02-online-courses/02-grpc-microservices/07-grpc-client-streaming/code/calculator/calculator_server/server.go
+++ 02-online-courses/02-grpc-microservices/07-grpc-client-streaming/code/calculator/calculator_server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -25,6 +26,7 @@ func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculat
 
 func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
 	fmt.Printf("Received PrimNumberDecomposition RPC: %v\n", req)
+
 	number := req.GetNumber()
 	divisor := int64(2)
 
@@ -40,6 +42,28 @@ func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositi
 		}
 	}
 	return nil
+}
+
+func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAverageServer) error {
+	fmt.Printf("Received ComputeAverage RPC\n")
+
+	sum := int32(0)
+	count := 0
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			average := float64(sum) / float64(count)
+			return stream.SendAndClose(&calculatorpb.ComputeAverageResponse{
+				Average: average,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error whilst reading client stream: %v", err)
+		}
+		sum += req.GetNumber()
+		count++
+	}
 }
 
 func main() {
